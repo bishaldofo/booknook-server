0617777a4647789e19b189a0488ac52fb863cd9b
Bishal Dofo <dofobishal@gmail.com>
2023-11-09 11:52:06 +0600

    Dot env added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000
@@ -28,7 +28,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     await client.connect();
+     
+
+     const serviceCollection = client.db('bookNook').collection('services');
+
+      app.get('/rooms', async (req, res) => {
+         const cursor = serviceCollection.find();
+         const result = await cursor.toArray();
+         res.send(result);
+      })
+
+      app.get('/rooms/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const result = await serviceCollection.findOne(query);
+         res.send(result);
+      })
+        
+        
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
